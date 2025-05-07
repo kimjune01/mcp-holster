c134431f26753ca8a22d561969540abf066183d7
June <kimjune01@gmail.com>
2025-05-07 13:33:19 -0700

    add ping with connectivity

--- holster.py
+++ holster.py
@@ -1,6 +1,7 @@
 from typing import Any
 import httpx
 from mcp.server.fastmcp import FastMCP
+import uvicorn
 
 # Initialize FastMCP server
 mcp = FastMCP("weather")
@@ -8,5 +9,9 @@ mcp = FastMCP("weather")
 
 @mcp.tool()
 async def ping() -> str:
-    """Ping the server"""
+    """Ping the Holster server"""
     return "Pong!"
+
+
+if __name__ == "__main__":
+    mcp.run()
